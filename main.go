4b8093d5102ae6309fc1dbf1c95f0b6936c0ab79
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-11-11 15:21:47 +0700

    only permit image file

--- main.go
+++ main.go
@@ -4,21 +4,45 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"os"
-
-	"io/fs"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
-var (
-	path     = flag.String("path", ".", "path to the folder to serve. Defaults to the current folder")
-	port     = flag.String("port", "8080", "port to serve on. Defaults to 8080")
-	username = "admin"
-	password = "password"
-)
+type ExtSlice []string
+
+func (list ExtSlice) Has(a string) bool {
+    for _, b := range list {
+        if (strings.HasSuffix(b, a)) {
+            return true
+        }
+    }
+    return false
+}
+
+
+var supported_types = ExtSlice{
+	"jpg",
+	"png",
+	"gif",
+}
+
+var Config struct {
+	Path     string
+	Port     string
+	Username string
+	Password string
+}
+
+func InitFlags() {
+	flag.StringVar(&Config.Path, "PATH", ".", "Path to the folder to serve. Defaults to the current folder")
+	flag.StringVar(&Config.Port, "PORT", "5000", "Port to serve on. Defaults to 5000")
+	flag.StringVar(&Config.Username, "USERNAME", "admin", "Username")
+	flag.StringVar(&Config.Password, "PASSWORD", "password", "Password")
+	flag.Parse()
+}
 
 func ContainsDotFile(name string) bool {
 	parts := strings.Split(name, "/")
@@ -34,158 +58,138 @@ type DotFileHidingFileSystem struct {
 	http.FileSystem
 }
 
-func (fileSystem DotFileHidingFileSystem) Open(name string) (http.File, error) {
+func (fs DotFileHidingFileSystem) Open(name string) (http.File, error) {
 	if ContainsDotFile(name) {
-		return nil, fs.ErrPermission
+		return nil, os.ErrPermission
 	}
-
-	file, err := fileSystem.FileSystem.Open(name)
+	file, err := fs.FileSystem.Open(name)
 	if err != nil {
 		return nil, err
 	}
-
-	stat, err := file.Stat()
-
-	if err != nil {
-		return nil, err
-	}
-
+	
+	stat, _ := file.Stat();
+	
 	if stat.IsDir() {
-		return nil, fs.ErrPermission
+		return nil, os.ErrPermission
 	}
 
-	fmt.Println("Get file " + name)
+	if !supported_types.Has(stat.Name()) {
 
-	return file, err
+	}
+
+
+	return file, nil
 }
 
-func Serve(dirname string, port string) error {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fileSystem := DotFileHidingFileSystem{http.Dir(dirname)}
-		if r.Method == http.MethodGet {
-			http.FileServer(fileSystem)
-		} else if r.Method == http.MethodPost {
-			BasicAuth(PostImage).ServeHTTP(w, r)
-		} else if r.Method == http.MethodDelete {
-			BasicAuth(DeleteImage).ServeHTTP(w, r)
-		} else {
-			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-			return
-		}
-	})
-
-	return http.ListenAndServe(":"+port, nil)
-}
-
-func BasicAuth(next http.HandlerFunc) http.HandlerFunc {
+func BasicAuth(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		u, p, ok := r.BasicAuth()
-		if !ok || !(u == username) || !(p == password) {
+		if !ok || u != Config.Username || p != Config.Password {
 			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
-		next.ServeHTTP(w, r)
+		handler(w, r)
+	}
+}
+
+func HandleRequest(dirname string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fileSystem := DotFileHidingFileSystem{http.Dir(dirname)}
+		switch r.Method {
+		case http.MethodGet:
+			http.FileServer(fileSystem).ServeHTTP(w, r)
+		case http.MethodPost:
+			BasicAuth(PostImage).ServeHTTP(w, r)
+		case http.MethodDelete:
+			BasicAuth(DeleteImage).ServeHTTP(w, r)
+		default:
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
 	}
 }
 
 func PostImage(w http.ResponseWriter, r *http.Request) {
 	dirPath := filepath.Dir(r.URL.Path)[1:]
-	err := os.MkdirAll(dirPath, 0755)
-	if err != nil {
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
 		http.Error(w, "Error creating folders: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	fileName := filepath.Base(r.URL.Path)
-	filePath := filepath.Join(dirPath, fileName)
+	filePath := filepath.Join(dirPath, filepath.Base(r.URL.Path))
 	file, err := os.Create(filePath)
 	if err != nil {
-		http.Error(w, "Error creating the file: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Error creating file: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
-	err = r.ParseMultipartForm(100 << 20) // 100MB
-	if err != nil {
-		http.Error(w, "Error parsing multipart form", http.StatusBadRequest)
+	if err := r.ParseMultipartForm(100 << 20); err != nil {
+		http.Error(w, "Error parsing form", http.StatusBadRequest)
 		return
 	}
 
 	fileUploaded, _, err := r.FormFile("file")
 	if err != nil {
-		http.Error(w, "Error retrieving the file", http.StatusBadRequest)
+		http.Error(w, "Error retrieving file", http.StatusBadRequest)
 		return
 	}
-	defer file.Close()
+	defer fileUploaded.Close()
 
 	if _, err := io.Copy(file, fileUploaded); err != nil {
-		http.Error(w, "Error saving the file", http.StatusInternalServerError)
+		http.Error(w, "Error saving file", http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Fprintln(w, "Successfully saved: "+r.URL.Path)
+	fmt.Fprintf(w, "Successfully saved: %s", r.URL.Path)
 }
 
 func DeleteImage(w http.ResponseWriter, r *http.Request) {
-  filePath := r.URL.Path[1:]
+	filePath := r.URL.Path[1:]
+	if err := os.Remove(filePath); err != nil {
+		http.Error(w, "Error deleting file: "+err.Error(), http.StatusBadRequest)
+		return
+	}
 
-  err := os.Remove(filePath)
-  if err != nil {
-      http.Error(w, "Error deleting file: "+err.Error(), http.StatusBadRequest)
-      return
-  }
+	dirPath := filepath.Dir(filePath)
+	for dirPath != "." {
+		isEmpty, err := IsDirEmpty(dirPath)
+		if err != nil {
+			http.Error(w, "Error checking directory: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if isEmpty {
+			if err = os.Remove(dirPath); err != nil {
+				http.Error(w, "Error deleting directory: "+err.Error(), http.StatusInternalServerError)
+				return
+			}
+		} else {
+			break
+		}
+		dirPath = filepath.Dir(dirPath)
+	}
 
-  dirPath := filepath.Dir(filePath)
-  for dirPath != "." {
-      isEmpty, err := isDirEmpty(dirPath)
-      if err != nil {
-          http.Error(w, "Error checking directory: "+err.Error(), http.StatusInternalServerError)
-          return
-      }
-      if isEmpty {
-          err = os.Remove(dirPath)
-          if err != nil {
-              http.Error(w, "Error deleting directory: "+err.Error(), http.StatusInternalServerError)
-              return
-          }
-      } else {
-          break
-      }
-      dirPath = filepath.Dir(dirPath)
-  }
-
-  fmt.Fprintln(w, "Successfully deleted: " + filePath)
+	fmt.Fprintf(w, "Successfully deleted: %s", filePath)
 }
 
-func isDirEmpty(name string) (bool, error) {
-  f, err := os.Open(name)
-  if err != nil {
-      return false, err
-  }
-  defer f.Close()
-
-  _, err = f.Readdirnames(1)
-  if err == io.EOF {
-      return true, nil
-  }
-  return false, err
+func IsDirEmpty(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+	_, err = f.Readdirnames(1)
+	return err == io.EOF, err
 }
 
 func main() {
-	flag.Parse()
-
-	dirname, err := filepath.Abs(*path)
+	InitFlags()
+	dirname, err := filepath.Abs(Config.Path)
 	if err != nil {
-		log.Fatalf("Could not get absolute path to directory: %s: %s", dirname, err.Error())
+		log.Fatalf("Could not get absolute path: %s", err)
 	}
-
-	log.Printf("Serving %s on port %s", dirname, *port)
-
-	err = Serve(dirname, *port)
-	if err != nil {
-		log.Fatalf("Could not serve directory: %s: %s", dirname, err.Error())
+	log.Printf("Serving %s on port %s", dirname, Config.Port)
+	if err := http.ListenAndServe(":"+Config.Port, HandleRequest(dirname)); err != nil {
+		log.Fatalf("Could not start server: %s", err)
 	}
-
 }
